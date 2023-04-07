7c1dcd67c6e72d4580555833227455b756e6d58c
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-04-07 17:22:59 -0300

    inicializando o projeto com class e package

--- src/main/java/com/starking/correios/controllers/CorreiosController.java
+++ src/main/java/com/starking/correios/controllers/CorreiosController.java
@@ -1,20 +1,23 @@
 package com.starking.correios.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.starking.correios.model.Address;
 
+import jakarta.websocket.server.PathParam;
+
 @RestController
-@RequestMapping
 public class CorreiosController {
 
+	@GetMapping("/status")
 	public String getStatus() {
 		return "up";
 	}
 
-	public Address getAddressByZipCode(String zipCode) {
-		return null;
+	@GetMapping("/zipcode/{zipcode}")
+	public Address getAddressByZipCode(@PathParam("zipcode") String zipcode) {
+		return Address.builder().zipCode(zipcode).build();
 	}
 
 }
--- src/main/java/com/starking/correios/model/Address.java
+++ src/main/java/com/starking/correios/model/Address.java
@@ -1,5 +1,10 @@
 package com.starking.correios.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +14,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "address")
 public class Address {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String zipCode;
 	private String street;
 	private String district;
